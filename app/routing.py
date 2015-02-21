8e3b5dd40dcff5655a13ff32b7f4c3f18e665c62
lymacasm <lyden.m.smith@gmail.com>
2015-02-21 14:26:27 -0800

    Update and rename app to app/routing.py

---
+++ app/routing.py
@@ -0,0 +1,18 @@
+import json
+
+class brains:
+	def start(self, data):
+		self.mInfo = json.load[data]
+		self.mWidth = self.info["width"]
+		self.mHeight = self.info["height"]
+		self.mGameID = self.info["game_id"]
+		print(self.mGameID)
+		self.Board[self.mWidth,self.mHeight]
+		
+	def move:
+		return json.dumps({
+        	'move': 'right',
+        	'taunt': 'battlesnake-python!'
+    	})
+	
+	
